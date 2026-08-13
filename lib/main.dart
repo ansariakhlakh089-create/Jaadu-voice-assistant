c4d165cf98f26e787e1e1ba7300336bf66344afb
ansariakhlakh089-create <ansariakhlakh089@gmail.com>
2026-08-13 11:52:43 +0530

    Fix URL formatting in openUrl function

--- lib/main.dart
+++ lib/main.dart
@@ -181,7 +181,7 @@ class _JaaduHomeState extends State<JaaduHome> {
       'गाने',
       'संगीत',
     ])) {
-      await openUrl('https://musicplay);
+      await openUrl('https://musicplay');
       return;
     }
 
